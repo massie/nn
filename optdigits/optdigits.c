e79c97995383f22d0402869b70302ff3441514d8
Matt Massie <massie@cs.berkeley.edu>
2016-10-08 18:04:14 -0700

    Updated code to use all ten of the output nodes

--- optdigits/optdigits.c
+++ optdigits/optdigits.c
@@ -6,14 +6,28 @@
 // This global is unfortunate and necessary since it can't be passed to the callback */
 static FILE *datafile = NULL;
 
-void
+static int probability_output_to_int(fann_type * output, int num_output)
+{
+	float probability = 0.0;
+	int val = 0;
+	unsigned int i;
+	for (i = 0; i < num_output; i++) {
+		if (output[i] > probability) {
+			val = i;
+			probability = output[i];
+		}
+	}
+	return val;
+}
+
+static void
 data_callback(unsigned int num_total, unsigned int num_input,
 	      unsigned int num_output, fann_type * inputs, fann_type * outputs)
 {
 	char line[512];
 	int i;
 	char *strval;
-	float fval;
+	long val;
 	char *delim = ",";
 	fgets(line, sizeof(line), datafile);
 	for (i = 0; i < num_input; i++) {
@@ -22,14 +36,14 @@ data_callback(unsigned int num_total, unsigned int num_input,
 		} else {
 			strval = strtok(NULL, delim);
 		}
-		fval = strtof(strval, NULL);
-		// fprintf(stderr, "%f|", fval);
-		inputs[i] = (fann_type) fval;
+		val = strtol(strval, NULL, 0);
+		// fprintf(stderr, "%d|", val);
+		inputs[i] = (fann_type) val;
 	}
 	strval = strtok(NULL, delim);
-	fval = strtof(strval, NULL);
-	// fprintf(stderr, "====%f\n\n", fval);
-	outputs[0] = fval / 10;
+	val = strtol(strval, NULL, 0);
+	memset(outputs, 0, sizeof(*outputs));
+	outputs[val] = 1;
 }
 
 int main(int argc, char *argv[])
@@ -38,11 +52,13 @@ int main(int argc, char *argv[])
 	const unsigned int num_output = 10;
 	const unsigned int num_layers = 3;
 	const unsigned int num_neurons_hidden = 25;
-	const float desired_error = (const float)0.0001;
-	const unsigned int max_epochs = 5000;
+	const float desired_error = (const float)0.00001;
+	const unsigned int max_epochs = 50000;
 	const unsigned int num_threads = 8;
 	unsigned int i;
+	unsigned int missed_tests;
 	float error = FLT_MAX;
+	fann_type *calc_out;
 
 	struct fann *ann =
 	    fann_create_standard(num_layers, num_input, num_neurons_hidden,
@@ -74,7 +90,24 @@ int main(int argc, char *argv[])
 					    data_callback);
 	fclose(datafile);
 
-	printf("MSE error on test data: %f\n", fann_test_data(ann, test_data));
+	printf("Running through %d test values\n",
+	       fann_length_train_data(test_data));
+	missed_tests = 0;
+	for (i = 0; i != fann_length_train_data(test_data); i++) {
+		calc_out = fann_run(ann, test_data->input[i]);
+		int calc_value =
+		    probability_output_to_int(calc_out, num_output);
+		int test_value =
+		    probability_output_to_int(test_data->output[i], num_output);
+		if (calc_value != test_value) {
+			missed_tests++;
+			printf("NN calculated %d, should be %d\n", calc_value,
+			       test_value);
+		}
+	}
+	printf("NN miscalculated %d of %d values in test set (%f %% error)\n",
+	       missed_tests, fann_length_train_data(test_data),
+	       (float)missed_tests / fann_length_train_data(test_data) * 100);
 
 	fann_save(ann, "optdigits.net");
 	fann_destroy(ann);
